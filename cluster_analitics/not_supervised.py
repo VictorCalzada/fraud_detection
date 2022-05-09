c4cfd82ff03f4e9bc4655c58d637e4a626f52ca9
VictorCalzada <victor.calzada.cabano@gmail.com>
2022-05-09 14:36:09 +0200

    Clases no supervisadas

--- cluster_analitics/not_supervised.py
+++ cluster_analitics/not_supervised.py
@@ -2,3 +2,30 @@ from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemblr import IsolationForest
 from minisom import MiniSom
 from sklearn.cluster import DBSCAN
+
+
+
+
+##########################################################
+#               Clases no supervisadas                   #
+########################################################## 
+class IsolationForest_Custom(IsolationForest):
+
+    def predict(self, X):
+        pred = super().predict(X)
+        scale_func = np.vectorize(lambda x: 1 if x == -1 else 0)
+        return scale_func(pred)
+
+
+
+class LocalOutlierFactor_Custom(LocalOutlierFactor):
+    pass
+
+
+class Som_Custom(MiniSom):
+    pass
+
+
+
+class Dbscan_Custom(DBSCAN):
+    pass
